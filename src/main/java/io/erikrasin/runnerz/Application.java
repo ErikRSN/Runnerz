83d71062afae4213557e1a471825306461bacd2b
ErikRSN <driedgrapes506@gmail.com>
2024-07-19 23:13:08 +0200

    first commit

---
+++ src/main/java/io/erikrasin/runnerz/Application.java
@@ -0,0 +1,13 @@
+package io.erikrasin.runnerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
